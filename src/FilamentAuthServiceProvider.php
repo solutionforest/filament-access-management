fab4fc7d9e819ffd6b5f592e145ac9a85c59ec73
Raphael Canguçu <raphael@codificar.com.br>
2024-01-20 00:12:16 -0300

    Update FilamentAuthServiceProvider.php

--- src/FilamentAuthServiceProvider.php
+++ src/FilamentAuthServiceProvider.php
@@ -2,8 +2,12 @@
 
 namespace SolutionForest\FilamentAccessManagement;
 
+use Filament\Facades\Filament;
+use Filament\Navigation\NavigationBuilder;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\ServiceProvider;
+use SolutionForest\FilamentAccessManagement\Facades\FilamentAuthenticate;
+use SolutionForest\FilamentAccessManagement\Http\Auth\Permission;
 
 class FilamentAuthServiceProvider extends ServiceProvider
 {
@@ -15,5 +19,50 @@ class FilamentAuthServiceProvider extends ServiceProvider
     public function register()
     {
         //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Filament::serving(function () {
+            $this->configureNavigation();
+            $this->configureComponent();
+        });
+    }
+
+    protected function configureNavigation()
+    {
+        if (config('filament-access-management.filament.navigation.enabled', false)) {
+            Filament::navigation(function (NavigationBuilder $builder) {
+                if ($customBuilder = FilamentAuthenticate::getCustomNavigation()) {
+                    $builder = $customBuilder;
+                }
+
+                return $builder->groups(FilamentAuthenticate::getUserNavigationGroups());
+            });
+        }
+    }
+
+    protected function configureComponent()
+    {
+        if (config('filament-access-management.filament.path_permission_checking.action', false)) {
+            \Filament\Support\Actions\BaseAction::configureUsing(function (\Filament\Support\Actions\BaseAction $component) {
+                if (method_exists($component, 'getUrl')) {
+                    $component->hidden(function () use ($component) {
+                        $url = $component->getUrl();
+
+                        if (empty($url)) {
+                            return false;
+                        }
+
+                        return ! Permission::checkPermission($url);
+                    });
+                }
+            });
+        }
     }
 }

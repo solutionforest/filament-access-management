2b289e33862b0a94bf3116bcc16a4d41acec4566
cklei-carly <68525320+cklei-carly@users.noreply.github.com>
2026-07-28 11:49:03 +0800

    feat: conditionally run upgrade command after v1 and collect errors gracefully

--- src/Commands/Upgrade.php
+++ src/Commands/Upgrade.php
@@ -2,6 +2,7 @@
 
 namespace SolutionForest\FilamentAccessManagement\Commands;
 
+use Composer\InstalledVersions;
 use Exception;
 use Illuminate\Console\Command;
 use SolutionForest\FilamentAccessManagement\Support\Utils;
@@ -14,14 +15,18 @@ class Upgrade extends Command
 
     public $description = 'Upgrade FilamentAccessManagement';
 
+    private const PACKAGE_NAME = 'solution-forest/filament-access-management';
+
     public function handle(): int
     {
-        $this->upgradeMenuUrl();
+        if ($this->isLocalDevVersion() || $this->isVersionGreaterThan('1.0.0')) {
+            $this->upgradeAfterV1();
+        }
 
         return static::SUCCESS;
     }
 
-    private function upgradeMenuUrl()
+    private function upgradeAfterV1()
     {
         $model = Utils::getMenuModel();
 
@@ -35,18 +40,16 @@ class Upgrade extends Command
             ->where('is_filament_panel', false) // default value
             ->get();
 
-        if ($v1PathRecords->isEmpty()) {
+        if (count($v1PathRecords) <= 0) {
             $this->info('No legacy menu uri to upgrade.');
 
             return;
         }
 
-        progress('Updating uri of menu as current version', count($v1PathRecords), function () use ($v1PathRecords) {
-
+        $errors = [];
+        progress('Updating uri of menu as current version', count($v1PathRecords), function ($index) use ($v1PathRecords, &$errors) {
             foreach ($v1PathRecords as $v1PathRecord) {
-
                 try {
-
                     $newUri = (string) str($v1PathRecord->uri)
                         ->replace('/admin', '');
 
@@ -54,12 +57,37 @@ class Upgrade extends Command
                         'uri' => $newUri,
                         'is_filament_panel' => true,
                     ]);
-
                 } catch (Exception $e) {
-                    $this->error("Updating uri of menu failed (Detail: {$e->getMessage()})");
+                    $errors[] = "[#{$index}] Updating uri of menu failed (Detail: {$e->getMessage()})";
                 }
             }
         });
 
+        if (count($errors) > 0) {
+            $this->error('Upgrade completed with errors:');
+            foreach ($errors as $error) {
+                $this->error($error);
+            }
+        }
+    }
+
+    private function getCurrentVersion(): string
+    {
+        return preg_replace(
+            '/\.x-dev$/',
+            '.0',
+            InstalledVersions::getPrettyVersion(self::PACKAGE_NAME)
+            ?? InstalledVersions::getVersion(self::PACKAGE_NAME)
+        );
+    }
+
+    private function isVersionGreaterThan(string $version): bool
+    {
+        return version_compare($this->getCurrentVersion(), $version, '>');
+    }
+
+    private function isLocalDevVersion(): bool
+    {
+        return str_starts_with($this->getCurrentVersion(), 'dev-') || str_ends_with($this->getCurrentVersion(), '.x-dev');
     }
 }
